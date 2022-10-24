f969c0101de069c8185acaf4d30a4604ccdbf7ea
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2022-10-24 10:11:16 -0300

    primeiro teste basico da api (sem bd e nem rotas)

--- api/index.jsx
+++ api/index.jsx
@@ -5,5 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extends: false }));
 
+app.get('/', (request, response) => {
+    response.send('Tudo ok');
+});
 
 app.listen(3001, () => console.log('running...'));
