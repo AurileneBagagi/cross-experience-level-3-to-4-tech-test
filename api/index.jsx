6b1cd53db22a2f5f9452cbd5ab548d960daac561
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2022-10-24 23:41:33 -0300

    Adicionando comentarios

--- api/index.jsx
+++ api/index.jsx
@@ -2,12 +2,12 @@
 const express = require('express');
 const routes = require('./routes/routes.js');
 
-const app = express();
+const app = express(); //  chamando o app como API
 
-
+/* Especificado funções/modulos a utilizar na api */
 app.use(express.json());
 app.use(express.urlencoded({ extends: false }));
 app.use(routes);
 
 
-app.listen(3001, () => console.log('Express started at http://localhost:3001'));
+app.listen(3001, () => console.log('Express started at http://localhost:3001')); //espeficicando a porta para a API
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,11 +1,14 @@
+//  "constatando" modulos para o uso 
 const  express = require('express');
-const fs = require('fs');
+const fs = require('fs'); // para trabalhar melhor com arquivos JSON
 const routes = express.Router()
-const Cards = require('../../resources/cards/available-cards.json'); //chamando o arquivo JSON onde contem os Card para os testes
-const Plans = require('../../resources/data/available-plans.json');
+const Cards = require('../../resources/cards/available-cards.json'); //chamando o arquivo JSON onde contem os Card para os testes de conexão e leitura
+const Plans = require('../../resources/data/available-plans.json'); //chamando o arquivo JSON onde contem os Planos para os testes de conexão e leitura
+/*chamando o arquivo JSON para servi como db do methodo responsavel por "armazenar" os dados dos cartões ------------------------------------*/
 var data = fs.readFileSync(__dirname +'/../database/farce.json');
 var Farcedb = JSON.parse(data);
 
+/* Method get (chamada dos dados do "servidor") ---------------------------------------------------------*/
 routes.get('/Plans', (request, response) => {
     return response.json(Plans);
 });
@@ -18,21 +21,21 @@ routes.get('/Farse', (request, response) => {
     return response.json(Farcedb);
 });
 
-routes.post('/addCard', (request, response) => {
-    const body = request.body;
+/* Method post (inserindo dados no "falso servidor") ---------------------------------------------------------*/
 
-    if (!body){
+routes.post('/addCard', (request, response) => {
+    const body = request.body; //recebendo valores
+
+    if (!body){ //se os valores não estiverem adequados
         return response.status(400).end();
     };
-    Farcedb.data.push(body);
-
+    Farcedb.data.push(body); //puchando o valor temporariamente
+    /* TESTE: Para saber se de fato armazenaria os dados------------------------------------------------------- */
     var newData = JSON.stringify (Farcedb);
-    
     fs.writeFile (__dirname +'/../database/farce.json', newData, err => {
-
-        if (err) console.log(err);
+        if (err) console.log(err); // imprime o erro caso exista um
         return response.json(Farcedb)});   
 });
 
 
-module.exports = routes
+module.exports = routes //exporta o arquivo
